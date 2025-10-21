686efcbd665f7a9c7b576b8da824a70da930cc65
HablasBlaBla <00001111987208sp@al.educacao.sp.gov.br>
2025-10-21 08:18:36 -0300

    importei o dotenv para usar as variáveis

--- config.php
+++ config.php
@@ -1,4 +1,7 @@
 <?php
+require_once 'vendor/autoload.php';
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
 // Configurações do Google OAuth2
 define('GOOGLE_CLIENT_ID', $_ENV['GOOGLE_CLIENT_ID']);
 define('GOOGLE_CLIENT_SECRET', $_ENV['GOOGLE_CLIENT_SECRET']);
